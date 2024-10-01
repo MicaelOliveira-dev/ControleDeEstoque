45fe4d9e1d138e330ea8c2d092a4a687da668979
MicaelOliveira-dev <micaeloliveiradev@hotmail.com>
2024-09-30 21:10:19 -0300

    Componente Button

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,10 +1,12 @@
+import Button from "./components/button";
 
 
 function App() {
+  
 
   return (
-      <h2 className="bg-blue-500 text-white">Teste Tailwind</h2>
+    <div></div>
   )
 }
 
-export default App
+export default App;
---
+++ Front/src/components/button.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface ButtonProps {
+  text: string;                      
+  onClick: () => void;               
+  disabled?: boolean;                
+}
+
+// Componente Button funcional com props
+const Button: React.FC<ButtonProps> = ({ text, onClick, disabled }) => {
+  return (
+    <button 
+      className='bg-[#7C3AED] font-["Titillium_Web"] text-white font-bold w-full md:w-[385px] h-[50px] md:h-[50px] rounded-md py-2 px-4'
+      onClick={onClick} 
+      disabled={disabled}
+    >
+      {text}
+    </button>
+
+  );
+};
+
+export default Button;
